eef13916a48de18f1a0b5f4a52019817f3b0edce
Amanda <amandajuhlsilberg@hotmail.com>
2025-09-24 15:10:55 +0200

    No more double RelayCommand

--- ReolmarkedTeam15/Helpers/RelayCommand.cs
+++ ReolmarkedTeam15/Helpers/RelayCommand.cs
@@ -7,35 +7,59 @@ using System.Windows.Input;
 
 namespace ReolmarkedTeam15.Helpers
 {
-    internal class RelayCommand : ICommand
-    // All from WPF Tutorial Video: <Link Coming>.
+    public class RelayCommand : ICommand
+    
     {
-        private Action<object> execute; //Function call
+      
+     // Action to execute when the command is triggered without parameters
+        private readonly Action _execute;
 
-        private Func<object, bool> canExecute; // FUNK! returns value of the type specified by the bool parameter.
-        public event EventHandler? CanExecuteChanged
+        // Action to execute when the command is triggered with a parameter
+        private readonly Action<object> _executeWithParam;
+
+        // Optional method to determine if the command can execute (e.g. enabled/disabled logic)
+        private readonly Func<bool> _canExecute;
+
+        // Constructor for commands that don't take parameters
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
         {
+            // Store the parameterless action and the canExecute condition (if any)
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        // Constructor for commands that take parameters
+        public RelayCommand(Action<object> executeWithParam)
+        {
+            // Store the action that takes an object as a parameter
+            _executeWithParam = executeWithParam;
+        }
+
+        // Determines if the command can execute (returns true/false)
+        public bool CanExecute(object parameter)
+        {
+            // If there's no specific condition, return true (allow command to execute)
+            return _canExecute == null || _canExecute();
+        }
+
+        // Executes the appropriate action (parameterless or with parameter)
+        public void Execute(object parameter)
+        {
+            // If the parameterless action is set, execute it
+            if (_execute != null)
+                _execute();
+            // If the action with parameters is set, execute it with the passed parameter
+            else
+                _executeWithParam(parameter);
+        }
+
+        // Event triggered when CanExecute changes (helps WPF know when the command's enabled state changes)
+        public event EventHandler CanExecuteChanged
+        {
+            // Add a handler for CanExecuteChanged (this gets triggered when we call CommandManager.RequerySuggested)
             add { CommandManager.RequerySuggested += value; }
+            // Remove the handler for CanExecuteChanged
             remove { CommandManager.RequerySuggested -= value; }
         }
-
-        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
-        {
-            this.execute = execute;
-            this.canExecute = canExecute;
-        }
-
-        public bool CanExecute(object? parameter)
-        {
-            return canExecute == null || canExecute(parameter);
-        }
-
-        public void Execute(object? parameter)
-        {
-            execute(parameter);
-
-        }
-
-     
     }
 }
