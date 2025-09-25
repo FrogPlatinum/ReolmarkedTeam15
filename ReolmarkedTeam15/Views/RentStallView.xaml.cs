a42a1b8202fb267e4bb853714ad513d0461724c8
Morten Lauritsen <m@morten-lauritsen.dk>
2025-09-25 12:42:42 +0200

    Added private field to hold selected stall from the StallView. Populated Combobox with Renters. 'Udlej' button to assign stall to selected renter + message window confirming.

--- ReolmarkedTeam15/Views/RentStallView.xaml.cs
+++ ReolmarkedTeam15/Views/RentStallView.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ReolmarkedTeam15.Models;
+using ReolmarkedTeam15.Repos;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +22,28 @@ namespace ReolmarkedTeam15.Views
     /// </summary>
     public partial class RentStallView : Window
     {
-        public RentStallView()
+        public ObservableCollection<Renter> Renters { get; set; }
+        public Renter SelectedRenter { get; set; }
+        private Stall _stallToRent;
+
+        public RentStallView(Stall stallToRent)
         {
             InitializeComponent();
+
+            var renterRepo = new MemoryRenterRepo(); // New instance of renter repository -> Shared repo??
+            Renters = new ObservableCollection<Renter>(renterRepo.GetAll());
+
+            _stallToRent = stallToRent;
+            DataContext = this;
+        }
+        // Assign renter to stall and change availability status
+        private void RentOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            _stallToRent.RenterID = SelectedRenter.RenterID;
+            _stallToRent.Availability = Stall.AvailabilityStatus.Optaget;
+
+            MessageBox.Show($"Reol {_stallToRent.StallID} udlejet til {SelectedRenter.RenterFirstName} {SelectedRenter.RenterLastName}!");
+            this.Close();
         }
     }
 }
