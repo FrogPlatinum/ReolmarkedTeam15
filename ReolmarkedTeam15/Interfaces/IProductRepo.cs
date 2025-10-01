b7c5e0868984424721f28e8ba5972ab5f889080b
Gavin Denver <gavindenver@gmail.com>
2025-10-01 11:37:20 +0200

    Implemented: - IProductRepo - ProductMemoryRepo - Product

---
+++ ReolmarkedTeam15/Interfaces/IProductRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReolmarkedTeam15.Models;
+
+namespace ReolmarkedTeam15.Interfaces
+{
+    public interface IProductRepo
+    {
+        public void Add(Product product);
+        public IEnumerable<Product> GetAll();
+        public Product GetById(int id);
+        public void Update(Product product);
+        public void Delete(Product product);
+    }
+}
